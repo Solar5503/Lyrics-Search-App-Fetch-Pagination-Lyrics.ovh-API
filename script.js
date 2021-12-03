2188f504ba1c2bc6dbddfc94a7a8b8fd43a6cc5c
Solar5503 <spidervvv@mail.ru>
2021-12-03 16:11:51 +0300

    Fix getLyrics

--- script.js
+++ script.js
@@ -56,7 +56,9 @@ async function getMoreSongs(url) {
 async function getLyrics(artist, songTitle) {
   const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
   const data = await res.json();
-  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+  const lyrics = data.lyrics
+    .replace(/[A-Z a-z]*(\r\n)/, '')
+    .replace(/(\r\n|\r|\n)/g, '<br>');
   result.innerHTML = `
   <h2><strong>${artist}</strong> - ${songTitle}</h2>
   <span>${lyrics}</span>
