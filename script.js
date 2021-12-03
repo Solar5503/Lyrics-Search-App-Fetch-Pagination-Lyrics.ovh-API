85f44b280ff51f5f068e98345f67e9a5aaee15dc
Solar5503 <spidervvv@mail.ru>
2021-12-03 13:39:56 +0300

    Update DOM & add pagination

--- script.js
+++ script.js
@@ -13,12 +13,51 @@ async function searchSongs(term) {
 }
 
 //Show song and artist in DOM
-function showData(data) {}
+function showData(data) {
+  result.innerHTML = `
+    <ul class='songs'>
+        ${data.data
+          .map(
+            (song) => `
+        <li>
+            <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+            <button class='btn' data-artist='${song.artist.name}' data-songtitle='${song.title}'>Get Lyrics</button>
+        </li>
+        `
+          )
+          .join('')}
+    </ul>
+  `;
+  if (data.prev || data.next) {
+    more.innerHTML = `
+        ${
+          data.prev
+            ? `<button class='btn' onclick="getMoreSongs('${data.prev}')">Prev</button>`
+            : ''
+        }
+        ${
+          data.next
+            ? `<button class='btn' onclick="getMoreSongs('${data.next}')">Next</button>`
+            : ''
+        }
+      `;
+  } else more.innerHTML = '';
+}
+
+//Get prev and next songs
+async function getMoreSongs(url) {
+  //   const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const res = await fetch(`https://api.codetabs.com/v1/proxy?quest=${url}`);
+  const data = await res.json();
+  showData(data);
+}
 
 //Event handlers
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const searchTerm = search.value.trim();
-  if (!searchTerm) alert('Please type in a search term');
-  else searchSongs(searchTerm);
+  if (!searchTerm) {
+    result.firstElementChild.innerText = 'Please type in a search term!';
+    more.innerHTML = '';
+  } else searchSongs(searchTerm);
 });
